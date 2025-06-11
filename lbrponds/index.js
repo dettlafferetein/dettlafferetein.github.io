1aa781f0d18e3b76556052f6d682287cfd4f0cc9
Dettlaff <90787612+dettlafferetein@users.noreply.github.com>
2025-06-11 15:45:10 -0400

    more

--- lbrponds/index.js
+++ lbrponds/index.js
@@ -31,13 +31,15 @@ function render() {
     const seed = parseInt(document.querySelector('#seed').value);
     const searches = parseInt(document.querySelector('#searches').value);
     const goggles = parseInt(document.querySelector('#goggles').value);
+    const rescount = parseInt(document.querySelector('#rescount').value);
+    const special = document.querySelector('#special').value;
 
     for (let i = 0; ; i++) {
-        if (window.ponds[i + seed + searches][goggles].match(/^Legendary/)) {
+        if (window.ponds[i + seed + searches][goggles].match(new RegExp("^Legendary:"+special))) {
             document.querySelector('#nextleg').textContent = `Your next Legendary is ${window.ponds[i + seed + searches][goggles].replace(/^Legendary:/g, '')} in ${i + 1} searches`;
             break;
         }
     }
 
-    document.querySelector('#result').innerHTML = window.ponds.slice(seed + searches, 1000 + seed + searches).map((v, k) => `<span class="${v[goggles].replace(':', ' ')}">${k + 1} (seed #${seed + searches + k}) - ${v[goggles].replace(':', ' ')}</span>`).join("");
+    document.querySelector('#result').innerHTML = window.ponds.slice(seed + searches, rescount + seed + searches).map((v, k) => `<span class="${v[goggles].replace(':', ' ')}">${k + 1} (seed #${seed + searches + k}) - ${v[goggles].replace(':', ' ')}</span>`).join("");
 }
