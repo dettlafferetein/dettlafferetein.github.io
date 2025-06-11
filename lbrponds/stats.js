8f1f1778fb0db48ccd58872f7963d4337518804a
Dettlaff <90787612+dettlafferetein@users.noreply.github.com>
2025-06-11 17:08:49 -0400

    stats

---
+++ lbrponds/stats.js
@@ -0,0 +1,21 @@
+
+document.addEventListener('DOMContentLoaded', async () => {
+    window.ponds = await (await fetch("ponds.json")).json();
+    render();
+
+    document.querySelector('#submit').addEventListener('click', e => {
+        e.preventDefault();
+        render();
+    });
+});
+
+function render() {
+    const special = document.querySelector('#special').value;
+
+    for (let i = 0; i < 6; i++) {
+        var res = window.ponds.map((v, k) => `<span class="${v[i].replace(':', ' ')}">seed #${k} - ${v[i].replace(':', ' ')}</span>`);
+        res = res.filter(v => v.match(new RegExp("^<span class=\"Legendary "+special)));
+        document.querySelector('#result'+i).innerHTML = res.join("");
+        document.querySelector('#result'+i+'name').innerHTML = `${i}/5 Goggle - ${res.length} seeds`;
+    }
+}
