3025a096926e712bd74205fae81837091c502f92
Dettlaff <90787612+dettlafferetein@users.noreply.github.com>
2025-06-12 13:09:41 -0400

    .

--- lbrponds/stats.js
+++ lbrponds/stats.js
@@ -11,10 +11,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 function render() {
     const special = document.querySelector('#special').value;
+    const rarity = document.querySelector('#rarity').value;
 
     for (let i = 0; i < 6; i++) {
         var res = window.ponds.map((v, k) => `<span class="${v[i].replace(':', ' ')}">seed #${k} - ${v[i].replace(':', ' ')}</span>`);
-        res = res.filter(v => v.match(new RegExp("^<span class=\"Legendary "+special)));
+        res = res.filter(v => v.match(new RegExp("^<span class=\"" + rarity + " " + special)));
         document.querySelector('#result'+i).innerHTML = res.join("");
         document.querySelector('#result'+i+'name').innerHTML = `${i}/5 Goggle - ${res.length} seeds`;
     }
